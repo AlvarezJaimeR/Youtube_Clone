7e6d2dcc3e307f5056bb3fb764d0fbd27d743618
Jaime Alvarez <jrralvarez@gmail.com>
2021-05-11 22:05:16 -0400

    base model is completed. added joi validation

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const Joi = require('Joi');
 
 const replySchema = new mongoose.Schema({
     text:{type: String, required: true, minlength:2, maxlength:300},
     likes:{type:Number, default:0},
     dislikes: {type:Number, default:0},
     date:{type: Date, default:Date.now}
-})
+});
 
 const commentSchema = new mongoose.Schema({
     text:{type: String, required: true, minlength:2, maxlength:300},
@@ -14,7 +15,35 @@ const commentSchema = new mongoose.Schema({
     replies:[{type: replySchema}],
     videoId: {type:String, required:true},
     date:{type:Date, default:Date.now}
-})
+});
 
-module.exports = mongoose.model("Comment", commentSchema);
-module.exports = mongoose.model("Reply", replySchema);
+const Comment = mongoose.model('Comment', commentSchema);
+const Reply = mongoose.model('Reply', replySchema);
+
+function validateComment (comment){
+    const schema = Joi.object({
+        text: Joi.string().min(2).max(300).required(),
+        likes: Joi.number(),
+        dislikes: Joi.number(),
+        videoId: Joi.string().required(),
+        date: Joi.date(),
+    });
+    return schema.validate(comment);
+}
+
+function validateReply (reply){
+    const schema = Joi.object({
+        text: Joi.string().min(2).max(300).required(),
+        likes: Joi.number(),
+        dislikes: Joi.number(),
+        date: Joi.date(),
+    });
+    return schema.validate(reply);
+}
+
+exports.Comment = Comment;
+exports.Reply = Reply;
+exports.commentSchema = commentSchema;
+exports.replySchema = replySchema;
+exports.validateComment = validateComment;
+exports.validateReply = validateReply;
---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const { Comment, Reply, validateComment, validateReply} = require ('../models/comment');
+
+
+//get all videos 
+
+//get related videos
+
+//post comment 
+
+//put for likes / dislikes
+
+//post reply 
